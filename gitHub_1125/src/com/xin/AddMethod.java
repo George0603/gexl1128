d2f5b4be9591f5a97f345f54490f483245b1d57b
George0603 <1152614219@qq.com>
2016-11-28 14:59:33 +0800

    添加了一个加法类

---
+++ gitHub_1125/src/com/xin/AddMethod.java
@@ -0,0 +1,15 @@
+package com.xin;
+
+public class AddMethod {
+
+	public static void main(String[] args) {
+		int x1 = 5 ;
+		int x2 = 6 ;
+		
+		System.out.println(add(x1,x2));
+	}
+	
+	public static int add(int x1, int x2){
+		return x1 + x2 ;
+	}
+}
