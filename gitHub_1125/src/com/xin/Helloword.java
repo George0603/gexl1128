1935dfc42bb18e9204ecc6ed0a8c55dc371a1a36
George0603 <1152614219@qq.com>
2016-11-28 14:32:38 +0800

    HelloWord
    Juse for learning .

--- gitHub_1125/src/com/xin/Helloword.java
+++ gitHub_1125/src/com/xin/Helloword.java
@@ -3,6 +3,6 @@ package com.xin;
 public class Helloword {
 
 	public static void main(String[] args) throws Exception{
-		System.out.println("Hello world !");
+		System.out.println("Hello world ! My name is George.");
 	}
 }
